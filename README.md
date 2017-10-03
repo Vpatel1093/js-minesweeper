63b86ed177cab609a974188a7f666ce21400b0a1
Vishal <v.patel1093@gmail.com>
2017-10-03 16:04:32 -0400

    initialize repo

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.theodinproject.com/courses/javascript-and-jquery/lessons/minesweeper?ref=lnav
+
+Preview here: https://htmlpreview.github.io/?https://github.com/vpatel1093/js-minesweeper/blob/master/index.html
---
+++ js/minesweeper.js
@@ -0,0 +1,130 @@
+// Code borrowed from js-snake as template
+
+// In game variables
+var snake = [];
+var score = 0;
+var multiplier = 1;
+var fruitCell = [];
+var gameInProgress = false;
+var currentDirection = '';
+var lastDirection = '';
+const left = 37;
+const up = 38;
+const right = 39;
+const down = 40;
+
+function initGrid(n) {
+  const rows = n;
+  const columns = n;
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+      $('#grid').append('<div id="row' + i + '-col' + j + '" class="cell empty"></div>');
+    };
+  };
+};
+
+
+function resetGame() {
+
+}
+
+function placeMines() {
+  // do {
+  //   var fruitRow = Math.floor(Math.random()*40);
+  //   var fruitColumn = Math.floor(Math.random()*40);
+  //   fruitCell = $('#row' + fruitRow + '-col' + fruitColumn);
+  // } while(fruitCell.hasClass('snake'));
+  // fruitCell.addClass('apple').removeClass('empty');
+};
+
+function playGame() {
+  // if (gameInProgress) {
+  //   setTimeout(moveSnake,100/(multiplier*.5));
+  //   $('h3').text("Score: " + score);
+  // };
+};
+
+function moveSnake() {
+  // $(document).keydown(function(newDirection) {
+  //   if (37 <= newDirection.which && newDirection.which <= 40) {
+  //     // Don't allow snake to make 180 direction change
+  //     if (Math.abs(newDirection.which - lastDirection) !== 2) {
+  //       currentDirection = newDirection.which;
+  //     };
+  //   };
+  // });
+  //
+  // var move = function(currentDirection) {
+  //   var snakeHead = snake[0];
+  //   switch(currentDirection){
+  //     case 37:
+  //       var nextCellCoords = [snakeHead[0], snakeHead[1]-1];
+  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+  //       break;
+  //     case 38:
+  //       var nextCellCoords = [snakeHead[0]-1, snakeHead[1]];
+  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+  //       break;
+  //     case 39:
+  //       var nextCellCoords = [snakeHead[0], snakeHead[1]+1];
+  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+  //       break;
+  //     case 40:
+  //       var nextCellCoords = [snakeHead[0]+1, snakeHead[1]];
+  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
+  //       break;
+  //   };
+  //
+  //   // Next cell is snake
+  //   if (nextCell.hasClass('snake')) {
+  //     alert('Game over! You hit your own snake!');
+  //     resetGame();
+  //     return;
+  //   };
+  //
+  //   // Next cell is wall
+  //   if (nextCellCoords[0] < 0 || nextCellCoords[0] > 39 || nextCellCoords[1] < 0 || nextCellCoords[1] > 39) {
+  //     alert('Game over! You hit a wall!');
+  //     resetGame();
+  //     return;
+  //   };
+  //
+  //   // Next cell is empty
+  //   if (nextCell.hasClass('empty')) {
+  //     snake.unshift(nextCellCoords);
+  //     nextCell.removeClass('empty').addClass('snake');
+  //     var snakeTail = snake.pop();
+  //     $('#row' + snakeTail[0] + '-col' + snakeTail[1]).removeClass('snake').addClass('empty');
+  //   };
+  //
+  //   // Next cell is apple
+  //   if (nextCell.hasClass('apple')) {
+  //     snake.unshift(nextCellCoords);
+  //     nextCell.removeClass('apple').addClass('snake');
+  //     multiplier += .05
+  //     placeFruit();
+  //     score = Math.floor(score += 10*multiplier);
+  //   };
+  // };
+  //
+  // move(currentDirection);
+  // lastDirection = currentDirection;
+  // playGame();
+};
+
+$(document).ready(function initGame() {
+  // initGrid();
+  // initSnake();
+  // placeFruit();
+  //
+  // $(document).keydown(function startGame(key) {
+  //   if (!gameInProgress) {
+  //     currentDirection = key.which;
+  //     if (currentDirection === left || currentDirection === up || currentDirection === right || currentDirection === down) {
+  //       gameInProgress = true;
+  //       playGame();
+  //     };
+  //   };
+  // });
+});
