15b87a61ac3aea31459558dad749cf039ef3b9bf
Vishal <v.patel1093@gmail.com>
2017-10-04 17:20:08 -0400

    create function for showing cell's count of adjacent mines

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,3 +1,7 @@
+// In game variable
+var gridDims = 0;
+const surroundingCellCoords = [ [-1,0], [-1,1], [0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1] ];
+
 function initGrid(n) {
   const rows = n;
   const columns = n;
@@ -9,19 +13,43 @@ function initGrid(n) {
   };
 };
 
-function placeMines(numMines, gridDims) {
+function placeMines(numMines) {
   for(var i = 0; i < numMines; i++) {
     do {
       var mineRow = Math.floor(Math.random()*gridDims);
-      var mineColumn = Math.floor(Math.random()*gridDims);
-      mineCell = $('#row' + mineRow + '-col' + mineColumn);
+      var mineCol = Math.floor(Math.random()*gridDims);
+      mineCell = $('#row' + mineRow + '-col' + mineCol);
     } while(mineCell.hasClass('mine'));
     mineCell.addClass('mine');
   };
 };
 
 function markNumbers() {
+  $('.mine').each(function () {
+    var surroundingCells = cellsSurrounding($(this))
+    surroundingCells.forEach(function(cell) {
+      if (cell.text() > 0) {
+        cell.text(parseInt(cell.text()) + 1)
+      } else if (cell.text() === '') {
+        cell.text(1);
+      };
+    });
+  });
+};
 
+function cellsSurrounding(mineCell) {
+  var surroundingCells = []
+  var mineIndex = mineCell.index();
+  var mineRow = Math.floor(mineIndex/gridDims);
+  var mineCol = mineIndex - (mineRow * gridDims);
+  surroundingCellCoords.forEach(function(coord) {
+    var surroundingCell = $('#row' + (mineRow + coord[0]) + '-col' + (mineCol + coord[1]));
+    surroundingCells.push(surroundingCell)
+  });
+  surroundingCells = surroundingCells.filter(function(cell) {
+    return cell.length > 0 && !cell.hasClass('mine');
+  });
+  return surroundingCells;
 };
 
 function playGame() {
@@ -101,38 +129,41 @@ function moveSnake() {
 
 $(document).ready(function initGame() {
   $('#easy').click(function startGame() {
+    gridDims = 9;
     var grid = $('#grid');
     grid.empty();
     grid.css('height', '180px');
     grid.css('width', '180px');
     grid.css('border','2px solid #000');
-    initGrid(9);
-    placeMines(10,9);
-    markNumbers();
+    initGrid(gridDims);
+    placeMines(10,gridDims);
+    markNumbers(gridDims);
     playGame();
   });
 
   $('#medium').click(function startGame() {
+    gridDims = 16;
     var grid = $('#grid');
     grid.empty();
     grid.css('height', '320px');
     grid.css('width', '320px');
     grid.css('border','2px solid #000');
-    initGrid(16);
-    placeMines(40,16);
-    markNumbers();
+    initGrid(gridDims);
+    placeMines(40,gridDims);
+    markNumbers(gridDims);
     playGame();
   });
 
   $('#hard').click(function startGame() {
+    gridDims = 24;
     var grid = $('#grid');
     grid.empty();
     grid.css('height', '480px');
     grid.css('width', '480px');
     grid.css('border','2px solid #000');
-    initGrid(24);
-    placeMines(99,24);
-    markNumbers();
+    initGrid(gridDims);
+    placeMines(99,gridDims);
+    markNumbers(gridDims);
     playGame();
   });
 });
