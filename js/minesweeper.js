749266ed446fd2d5560702f794242d53d082cc1a
Vishal <v.patel1093@gmail.com>
2017-10-04 17:48:59 -0400

    hide details of unclicked cells

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -8,7 +8,7 @@ function initGrid(n) {
 
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
-      $('#grid').append('<div id="row' + i + '-col' + j + '" class="cell"></div>');
+      $('#grid').append('<div id="row' + i + '-col' + j + '" class="cell unclicked"></div>');
     };
   };
 };
@@ -30,6 +30,7 @@ function markNumbers() {
     surroundingCells.forEach(function(cell) {
       if (cell.text() > 0) {
         cell.text(parseInt(cell.text()) + 1)
+        cell.addClass('numbered');
       } else if (cell.text() === '') {
         cell.text(1);
       };
@@ -57,74 +58,6 @@ function playGame() {
   //   setTimeout(moveSnake,100/(multiplier*.5));
   //   $('h3').text("Score: " + score);
   // };
-};
-
-function moveSnake() {
-  // $(document).keydown(function(newDirection) {
-  //   if (37 <= newDirection.which && newDirection.which <= 40) {
-  //     // Don't allow snake to make 180 direction change
-  //     if (Math.abs(newDirection.which - lastDirection) !== 2) {
-  //       currentDirection = newDirection.which;
-  //     };
-  //   };
-  // });
-  //
-  // var move = function(currentDirection) {
-  //   var snakeHead = snake[0];
-  //   switch(currentDirection){
-  //     case 37:
-  //       var nextCellCoords = [snakeHead[0], snakeHead[1]-1];
-  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
-  //       break;
-  //     case 38:
-  //       var nextCellCoords = [snakeHead[0]-1, snakeHead[1]];
-  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
-  //       break;
-  //     case 39:
-  //       var nextCellCoords = [snakeHead[0], snakeHead[1]+1];
-  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
-  //       break;
-  //     case 40:
-  //       var nextCellCoords = [snakeHead[0]+1, snakeHead[1]];
-  //       var nextCell = $('#row' + nextCellCoords[0] + '-col' + nextCellCoords[1]);
-  //       break;
-  //   };
-  //
-  //   // Next cell is snake
-  //   if (nextCell.hasClass('snake')) {
-  //     alert('Game over! You hit your own snake!');
-  //     resetGame();
-  //     return;
-  //   };
-  //
-  //   // Next cell is wall
-  //   if (nextCellCoords[0] < 0 || nextCellCoords[0] > 39 || nextCellCoords[1] < 0 || nextCellCoords[1] > 39) {
-  //     alert('Game over! You hit a wall!');
-  //     resetGame();
-  //     return;
-  //   };
-  //
-  //   // Next cell is empty
-  //   if (nextCell.hasClass('empty')) {
-  //     snake.unshift(nextCellCoords);
-  //     nextCell.removeClass('empty').addClass('snake');
-  //     var snakeTail = snake.pop();
-  //     $('#row' + snakeTail[0] + '-col' + snakeTail[1]).removeClass('snake').addClass('empty');
-  //   };
-  //
-  //   // Next cell is apple
-  //   if (nextCell.hasClass('apple')) {
-  //     snake.unshift(nextCellCoords);
-  //     nextCell.removeClass('apple').addClass('snake');
-  //     multiplier += .05
-  //     placeFruit();
-  //     score = Math.floor(score += 10*multiplier);
-  //   };
-  // };
-  //
-  // move(currentDirection);
-  // lastDirection = currentDirection;
-  // playGame();
 };
 
 $(document).ready(function initGame() {
